901041676afd74d539b9e2467c42e85d9d51a0b4
Kris Thornton <Kristhornton@ymail.com>
2023-10-17 17:15:21 -0700

    Finished project all tests passing

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -6,6 +6,8 @@
   If no fallback is provided, TESTS WON'T WORK and other
   developers cloning this repo won't be able to run the project as is.
  */
-module.exports = {
-
-}
+  module.exports = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 9000,
+    JWT_SECRET: process.env.JWT_SECRET || 'shh'
+  }
